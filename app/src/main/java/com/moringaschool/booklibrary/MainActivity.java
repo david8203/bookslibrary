fd8db099a3608cc62e22c5ffb4c19864025d3ce2
David <kingdice@Atomic>
2022-06-07 11:31:30 +0300

    here we create an intent to travel from our main activity ro our bookactivity carrying our location object.

--- app/src/main/java/com/moringaschool/booklibrary/MainActivity.java
+++ app/src/main/java/com/moringaschool/booklibrary/MainActivity.java
@@ -2,18 +2,35 @@ package com.moringaschool.booklibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.findBooksButton)
-    Button mFindBooksButton;
-    @BindView(R.id.locationEditText)
-    EditText mLocationEditText;
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
+    @BindView(R.id.findBooksButton) Button mFindBooksButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindBooksButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == mFindBooksButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, BooksActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
