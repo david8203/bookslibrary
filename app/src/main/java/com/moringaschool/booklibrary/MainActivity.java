3e8ef8440d78888794b3a2eaf46aed5508d0dfc4
David <kingdice@Atomic>
2022-06-07 11:24:23 +0300

    we then add butterknife in our main activity

--- app/src/main/java/com/moringaschool/booklibrary/MainActivity.java
+++ app/src/main/java/com/moringaschool/booklibrary/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.findBooksButton)
+    Button mFindBooksButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
