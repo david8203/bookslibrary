0632a4ad3e1e7afebd5186771611847ca844c930
David <kingdice@Atomic>
2022-06-07 11:40:59 +0300

    here we are creating an array adapter to display our books we add our constructor and our output message.

--- app/src/main/java/com/moringaschool/booklibrary/MyBooksArrayAdapter.java
+++ app/src/main/java/com/moringaschool/booklibrary/MyBooksArrayAdapter.java
@@ -1,4 +1,27 @@
 package com.moringaschool.booklibrary;
 
-public class MyBooksArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyBooksArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mBooks;
+    private String[] mTitles;
+
+    public MyBooksArrayAdapter( Context mContext, int resource, String[] mBooks, String[] mTitles) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mBooks = mBooks;
+        this.mTitles = mTitles;
+    }
+    @Override
+    public Object getItem(int position) {
+        String books = mBooks[position];
+        String title = mTitles[position];
+        return String.format("%s \nYou should read this perhaps and leave a comment behind: %s", books, title);
+    }
+    @Override
+    public int getCount() {
+        return mBooks.length;
+    }
 }
